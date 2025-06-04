cd42819fa6a5fec0cfe1f53f975e921f01ecd896
Your Name <your-email@example.com>
2025-06-04 16:42:23 +0300

    created upload folder

---
+++ src/components/uploadFile/uploadfile.jsx
@@ -0,0 +1,17 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import "./uploadfile.css";
+
+
+
+const UploadFile = () => {
+  return (
+   <>
+   
+   </>
+  );
+};
+
+export default UploadFile;
