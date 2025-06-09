dd7fb26c0d639e32fa011242bf71b755d6d0f69c
Your Name <your-email@example.com>
2025-06-10 01:40:22 +0300

    edit on structure of upload.jsx and finish the new desing

--- src/components/uploadFile/uploadfile.jsx
+++ src/components/uploadFile/uploadfile.jsx
@@ -1,41 +1,42 @@
-import { NavLink, useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa6";
 import "./uploadfile.css";
 
+const handleResend = () => {
+  // Your logic to resend the verification email
+  console.log("Resend email triggered");
+};
+
+const upload = () => {
+  return (
+    <div className="signup-wrapper">
+  <div className="paragraphs-box"> 
+                    <p className="middle-paragraph">An email has been sent to the address provided. Please check your inbox.</p>
+                    <p className="middle-paragraph">Click the link in the email to confirm your account and complete the sign-up process.</p>
+                    <p className="middle-paragraph hyperlink"> Haven't received the email?
+                      <a href="#" onClick={handleResend} className="link-lickble">Click here to resend.</a></p> 
+      </div>
+        <div className="circle-container">  
+           <div className="circle-right"></div>
+              <div className="singup-arrowButton">
+
+              
+      
 
 
-const UploadFile = () => {
-
-
-return (
-   <div className="register-formSingup">
-
-      <p className="title-head">Create Account</p>
-
-      <div className="register-inputsSingup"></div>
-
-
-   
-    <div className="register-buttons-singupUpload">
-               
-   
-   
-                <button
-                 className="buttonSubmit-content-or"
-                 type="submit" >
-                 <FaArrowRight className="arrowRight" />
-   
-               </button>
-             </div>
-
-    <h1 className="signupForm-labelUpload"></h1>
-
-
+                <h1 className="signup"></h1>
+                    <button
+                      className="circle-button"
+                      type="submit">
+                      <FaArrowRight className="arrowRight" />
+                    </button>
+                </div>
+         </div>
+          <div className="">
+              <p className="title-head-signup">Create Account</p>
+          </div>
     </div>
   );
 };
 
-export default UploadFile;
+export default upload;
