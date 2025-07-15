0187f9946c801a5644845381776017207984d5b7
Your Name <your-email@example.com>
2025-07-15 23:52:53 +0300

    edit path

--- src/components/singUp/singup.jsx
+++ src/components/singUp/singup.jsx
@@ -50,7 +50,7 @@ const handleSubmit = async (e) => {
 
   setIsLoading(true);
   try {
-    const response = await axios.post(`${BASE_URL}/users/register`, form);
+    const response = await axios.post(`${BASE_URL}users/register`, form);
     const { userId, verificationToken } = response.data;
 
 
