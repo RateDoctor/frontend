12d7ed36183e03691ca3a2d5a119f65c375e1ff0
Your Name <your-email@example.com>
2025-09-05 21:49:20 +0300

    fix checking

--- src/components/checking/checking.jsx
+++ src/components/checking/checking.jsx
@@ -48,10 +48,10 @@ useEffect(() => {
       <MdOutlineKeyboardArrowLeft  className="wrapper-arrow-left"/>
       
   <div className="paragraphs-box"> 
-                    <p className="middle-paragraph">An email has been sent to the address provided. Please check your inbox.</p>
-                    <p className="middle-paragraph">Click the link in the email to confirm your account and complete the sign-up process.</p>
-                    <p className="middle-paragraph hyperlink"> Haven't received the email?
-                      <strong  onClick={handleResend} className="link-lickble">Click here to resend.</strong></p> 
+      <p className="middle-paragraph">An email has been sent to the address provided. Please check your inbox.</p>
+      <p className="middle-paragraph">Click the link in the email to confirm your account and complete the sign-up process.</p>
+      <p className="middle-paragraph hyperlink"> Haven't received the email?
+      <strong  onClick={handleResend} className="link-lickble">Click here to resend.</strong></p> 
       </div>
         <div className="circle-container">  
            <div className="circle-right"></div>
